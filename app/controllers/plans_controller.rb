6d92b3e6067f462fec16b79b30bd65d6d6fd3ead
serpel <serpel.js@gmail.com>
2016-02-03 23:10:22 -0600

    Custom print report for 10 & 11 grade

--- app/controllers/plans_controller.rb
+++ app/controllers/plans_controller.rb
@@ -126,65 +126,144 @@ class PlansController < ApplicationController
     end
   end
 
+  def excel_file_gradebook_for_10grade(plan_id)
+
+    plan ||= Plan.find(plan_id)
+    students ||= Student.find_all_by_batch_id(plan.subject.batch,
+                                              :conditions => {:is_deleted => false},
+                                              :order => "lower(gender) asc, lower(first_name) asc, lower(last_name) asc")
+
+    book = Spreadsheet::Workbook.new
+    sheet1 = book.create_worksheet :name => "#{t('grade')}"
+
+    # format header
+    format = Spreadsheet::Format.new :size => 12,
+                                     :weight => :bold,
+                                     :pattern => 1,
+                                     :pattern_fg_color => :silver,
+                                     :bottom => :thin,
+                                     :top => :thin
+    #puts format.inspect
+    sheet1.row(0).default_format = format
+    sheet1.row(1).default_format = format
+
+    #header sheet1
+    sheet1.update_row 0, '','','','','P1','','','P2','','','P3','','','P4'
+    row = sheet1.row 1
+    row.push "#{t('id')}", "#{t('name')}"
+    4.times do |i|
+      row.push "#{t('exam'+(i+1).to_s)}"
+      row.push "#{t('acum'+(i+1).to_s)}"
+      row.push "#{t('recov'+(i+1).to_s)}"
+      row.push "#{t('total')}"
+      if i+1 == 2
+        row.push "#{t('recovery')}"
+      end
+    end
+    row.push "#{t('recovery')}"
+    row.push "#{t('average')}"
+
+    #content
+    students.each_with_index do |student, i|
+      nota = student.notas.find_by_subject_id(plan.subject)
+      nota = set_students_notes_equal_zero(plan.subject.id, student.id) if nota.nil?
+
+      total = nota.total_average_special.round
+      sheet1.update_row i+2, student.id, student.full_name.to_s.to_my_utf8,
+                        nota.examen(1), nota.acumulado(1), nota.recuperacion(1), nota.total_parcial(1).round,
+                        nota.examen(2), nota.acumulado(2), nota.recuperacion(2), nota.total_parcial(2).round,
+                        nota.get_recovery(1),
+                        nota.examen(3), nota.acumulado(3), nota.recuperacion(3), nota.total_parcial(3).round,
+                        nota.examen(4), nota.acumulado(4), nota.recuperacion(4), nota.total_parcial(4).round,
+                        nota.get_recovery(2),
+                        total
+    end
+
+    sheet2 = book.create_worksheet :name => "#{t('detail')}"
+    row = sheet2.row 0
+    row.push "#{t('subject_code')}", "#{t('subject_name')}"
+    4.times do |i|
+      row.push "#{t('exam'+(i+1).to_s)}"
+      row.push "#{t('acum'+(i+1).to_s)}"
+      row.push "#{t('recov'+(i+1).to_s)}"
+    end
+    sheet2.update_row 1, plan.subject.id, plan.subject.name, plan.examen_1, plan.acumulado_1, plan.examen_2, plan.acumulado_2,
+                      plan.examen_3, plan.acumulado_3, plan.examen_4, plan.acumulado_4
+
+    return book
+  end
+
+  def excel_file_gradebook(plan_id)
+
+    plan ||= Plan.find(plan_id)
+    students ||= Student.find_all_by_batch_id(plan.subject.batch,
+                                              :conditions => {:is_deleted => false},
+                                              :order => "lower(gender) asc, lower(first_name) asc, lower(last_name) asc")
+
+    book = Spreadsheet::Workbook.new
+    sheet1 = book.create_worksheet :name => "#{t('grade')}"
+
+    # format header
+    format = Spreadsheet::Format.new :size => 12,
+                                     :weight => :bold,
+                                     :pattern => 1,
+                                     :pattern_fg_color => :silver,
+                                     :bottom => :thin,
+                                     :top => :thin
+    #puts format.inspect
+    sheet1.row(0).default_format = format
+    sheet1.row(1).default_format = format
+
+    #header sheet1
+    sheet1.update_row 0, '','','','','P1','','','P2','','','P3','','','P4'
+    row = sheet1.row 1
+    row.push "#{t('id')}", "#{t('name')}"
+    4.times do |i|
+      row.push "#{t('exam'+(i+1).to_s)}"
+      row.push "#{t('acum'+(i+1).to_s)}"
+      row.push "#{t('recov'+(i+1).to_s)}"
+      row.push "#{t('total')}"
+    end
+    row.push "#{t('recovery')}"
+    row.push "#{t('average')}"
+
+    #content
+    students.each_with_index do |student, i|
+      nota = student.notas.find_by_subject_id(plan.subject)
+      nota = set_students_notes_equal_zero(plan.subject.id, student.id) if nota.nil?
+
+      sheet1.update_row i+2, student.id, student.full_name.to_s.to_my_utf8,
+                        nota.examen(1), nota.acumulado(1), nota.recuperacion(1), nota.total_parcial(1).round,
+                        nota.examen(2), nota.acumulado(2), nota.recuperacion(2), nota.total_parcial(2).round,
+                        nota.examen(3), nota.acumulado(3), nota.recuperacion(3), nota.total_parcial(3).round,
+                        nota.examen(4), nota.acumulado(4), nota.recuperacion(4), nota.total_parcial(4).round,
+                        nota.get_recovery(1),
+                        nota.total_average
+    end
+
+    sheet2 = book.create_worksheet :name => "#{t('detail')}"
+    row = sheet2.row 0
+    row.push "#{t('subject_code')}", "#{t('subject_name')}"
+    4.times do |i|
+      row.push "#{t('exam'+(i+1).to_s)}"
+      row.push "#{t('acum'+(i+1).to_s)}"
+      row.push "#{t('recov'+(i+1).to_s)}"
+    end
+    sheet2.update_row 1, plan.subject.id, plan.subject.name, plan.examen_1, plan.acumulado_1, plan.examen_2, plan.acumulado_2,
+                      plan.examen_3, plan.acumulado_3, plan.examen_4, plan.acumulado_4
+
+    return book
+  end
+
   def export
     id = params[:id]
 
     if params[:id].present?
       plan ||= Plan.find(id)
-      students ||= Student.find_all_by_batch_id(plan.subject.batch,
-                                           :conditions => {:is_deleted => false},
-                                           :order => "lower(gender) asc, lower(first_name) asc, lower(last_name) asc")
 
-      book = Spreadsheet::Workbook.new
-      sheet1 = book.create_worksheet :name => "#{t('grade')}"
-
-      # format header
-      format = Spreadsheet::Format.new :size => 12,
-                                       :weight => :bold,
-                                       :pattern => 1,
-                                       :pattern_fg_color => :silver,
-                                       :bottom => :thin,
-                                       :top => :thin
-      #puts format.inspect
-      sheet1.row(0).default_format = format
-      sheet1.row(1).default_format = format
-
-      #header sheet1
-      sheet1.update_row 0, '','','','','P1','','','P2','','','P3','','','P4'
-      row = sheet1.row 1
-      row.push "#{t('id')}", "#{t('name')}"
-      4.times do |i|
-        row.push "#{t('exam'+(i+1).to_s)}"
-        row.push "#{t('acum'+(i+1).to_s)}"
-        row.push "#{t('recov'+(i+1).to_s)}"
-        row.push "#{t('total')}"
-      end
-      row.push "#{t('recovery')}"
-      row.push "#{t('average')}"
-
-      #content
-      students.each_with_index do |student, i|
-        nota = student.notas.find_by_subject_id(plan.subject)
-        nota = set_students_notes_equal_zero(plan.subject.id, student.id) if nota.nil?
-        sheet1.update_row i+2, student.id, student.full_name.to_s.to_my_utf8,
-                          nota.examen_1, nota.acumulado_1, nota.recuperacion_1, nota.total_parcial(1),
-                          nota.examen_2, nota.acumulado_2, nota.recuperacion_1, nota.total_parcial(2),
-                          nota.examen_3, nota.acumulado_3, nota.recuperacion_1, nota.total_parcial(3),
-                          nota.examen_4, nota.acumulado_4, nota.recuperacion_1, nota.total_parcial(4),
-                          nota.recovery,
-                          nota.total_average
-      end
-
-      sheet2 = book.create_worksheet :name => "#{t('detail')}"
-      row = sheet2.row 0
-      row.push "#{t('subject_code')}", "#{t('subject_name')}"
-      4.times do |i|
-        row.push "#{t('exam'+(i+1).to_s)}"
-        row.push "#{t('acum'+(i+1).to_s)}"
-        row.push "#{t('recov'+(i+1).to_s)}"
-      end
-      sheet2.update_row 1, plan.subject.id, plan.subject.name, plan.examen_1, plan.acumulado_1, plan.examen_2, plan.acumulado_2,
-                        plan.examen_3, plan.acumulado_3, plan.examen_4, plan.acumulado_4
+      ## La calificacion varia dependiendo de la escuela en este caso el grado 10 y 11
+      book = (plan.subject.batch.id == 40 or plan.subject.batch.id == 53) ?
+          excel_file_gradebook_for_10grade(plan.id) : excel_file_gradebook(plan.id)
 
       t = DateTime.now
       date = t.strftime("%Y%m%d")
@@ -201,22 +280,10 @@ class PlansController < ApplicationController
     end
   end
 
-  require 'spreadsheet'
-  def import
+  def import_gradebook(book, subject_id)
 
-    @errors ||= []
-    if request.post? && params[:file].present?
-      uploaded_io = params[:file]
-      file_path = copy_file_to_public uploaded_io
-
-      book = Spreadsheet.open file_path
-      sheet ||= book.worksheet 1
-
-      #puts sheet.inspect
-      subject_id = sheet.rows[1][0] if sheet.rows.size > 0
-
-      sheet = nil
       sheet ||= book.worksheet 0
+
       sheet.each 2 do |row|
         student = Student.find_by_id(row[0])
         nota = Nota.find_by_subject_id_and_student_id(subject_id,row[0]) || Nota.new
@@ -236,8 +303,60 @@ class PlansController < ApplicationController
         nota.subject_id = subject_id if nota.subject_id.nil?
         nota.student_id = row[0].to_i if nota.student_id.nil?
         nota.save unless student.nil?
-        (@errors.push nota.errors) unless nota.errors.empty?
       end
+      return book
+  end
+
+  def import_gradebook_for_10grade(book,  subject_id)
+      sheet ||= book.worksheet 0
+
+      sheet.each 2 do |row|
+        student = Student.find_by_id(row[0])
+        nota = Nota.find_by_subject_id_and_student_id(subject_id,row[0]) || Nota.new
+        nota.examen_1 = row[2].to_f
+        nota.acumulado_1 = row[3].to_f
+        nota.recuperacion_1 = row[4].to_f
+
+        nota.examen_2 = row[6].to_f
+        nota.acumulado_2 = row[7].to_f
+        nota.recuperacion_2 = row[8].to_f
+        nota.recovery = row[10].to_f
+
+        nota.examen_3 = row[11].to_f
+        nota.acumulado_3 = row[12].to_f
+        nota.recuperacion_3 = row[13].to_f
+
+        nota.examen_4 = row[15].to_f
+        nota.acumulado_4 = row[16].to_f
+        nota.recuperacion_4 = row[17].to_f
+        nota.recovery2 = row[19].to_f
+
+        nota.subject_id = subject_id if nota.subject_id.nil?
+        nota.student_id = row[0].to_i if nota.student_id.nil?
+        nota.save unless student.nil?
+      end
+
+      return book
+  end
+
+  require 'spreadsheet'
+  def import
+
+    @errors ||= []
+    if request.post? && params[:file].present?
+      uploaded_io = params[:file]
+      file_path = copy_file_to_public uploaded_io
+
+      book = Spreadsheet.open file_path
+      sheet ||= book.worksheet 1
+
+      #puts sheet.inspect
+      subject_id = sheet.rows[1][0] if sheet.rows.size > 0
+
+      subject = Subject.find subject_id
+      result = (subject.batch_id == 40 or subject.batch_id == 53) ?
+          import_gradebook_for_10grade(book, subject_id) : import_gradebook(book, subject_id)
+
       delete_file file_path
       flash[:notice] = "#{t('completed_text')}\n"
       redirect_to :back
