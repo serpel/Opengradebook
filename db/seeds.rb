f042a8ede09f50febb7ef169e65388136034d2d0
serpel <serpel.js@gmail.com>
2016-06-21 09:55:06 -0600

    seed file fix for my friend Ishant

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,30 +1,30 @@
 [
-  {"config_key" => "InstitutionName"                 ,"config_value" => "" },
-  {"config_key" => "InstitutionAddress"              ,"config_value" => ""},
-  {"config_key" => "InstitutionPhoneNo"              ,"config_value" => ""},
-  {"config_key" => "StudentAttendanceType"           ,"config_value" => "Daily"},
-  {"config_key" => "CurrencyType"                    ,"config_value" => "L"},
-  {"config_key" => "Locale"                          ,"config_value" => "es"},
-  {"config_key" => "AdmissionNumberAutoIncrement"    ,"config_value" => "1"},
-  {"config_key" => "EmployeeNumberAutoIncrement"     ,"config_value" => "1"},
-  {"config_key" => "TotalSmsCount"                   ,"config_value" => "0"},
-  {"config_key" => "NetworkState"                    ,"config_value" => "Online"},
-  {"config_key" => "FinancialYearStartDate"          ,"config_value" => Date.today},
-  {"config_key" => "FinancialYearEndDate"            ,"config_value" => Date.today+1.year},
-  {"config_key" => "AutomaticLeaveReset"             ,"config_value" => "0"},
-  {"config_key" => "LeaveResetPeriod"                ,"config_value" => "4"},
-  {"config_key" => "LastAutoLeaveReset"              ,"config_value" => nil},
-  {"config_key" => "GPA"                             ,"config_value" => "0"},
-  {"config_key" => "CWA"                             ,"config_value" => "0"},
-  {"config_key" => "CCE"                             ,"config_value" => "0"},
-  {"config_key" => "DefaultCountry"                  ,"config_value" => "73"}
+    {"config_key" => "InstitutionName"                 ,"config_value" => "" },
+    {"config_key" => "InstitutionAddress"              ,"config_value" => ""},
+    {"config_key" => "InstitutionPhoneNo"              ,"config_value" => ""},
+    {"config_key" => "StudentAttendanceType"           ,"config_value" => "Daily"},
+    {"config_key" => "CurrencyType"                    ,"config_value" => "L"},
+    {"config_key" => "Locale"                          ,"config_value" => "es"},
+    {"config_key" => "AdmissionNumberAutoIncrement"    ,"config_value" => "1"},
+    {"config_key" => "EmployeeNumberAutoIncrement"     ,"config_value" => "1"},
+    {"config_key" => "TotalSmsCount"                   ,"config_value" => "0"},
+    {"config_key" => "NetworkState"                    ,"config_value" => "Online"},
+    {"config_key" => "FinancialYearStartDate"          ,"config_value" => Date.today},
+    {"config_key" => "FinancialYearEndDate"            ,"config_value" => Date.today+1.year},
+    {"config_key" => "AutomaticLeaveReset"             ,"config_value" => "0"},
+    {"config_key" => "LeaveResetPeriod"                ,"config_value" => "4"},
+    {"config_key" => "LastAutoLeaveReset"              ,"config_value" => nil},
+    {"config_key" => "GPA"                             ,"config_value" => "0"},
+    {"config_key" => "CWA"                             ,"config_value" => "0"},
+    {"config_key" => "CCE"                             ,"config_value" => "0"},
+    {"config_key" => "DefaultCountry"                  ,"config_value" => "73"}
 ].each do |param|
   Configuration.find_or_create_by_config_key(param)
 end
 
 [
-  {"config_key" => "AvailableModules"                ,"config_value" => "HR"},
-  {"config_key" => "AvailableModules"                ,"config_value" => "Finance"}
+    {"config_key" => "AvailableModules"                ,"config_value" => "HR"},
+    {"config_key" => "AvailableModules"                ,"config_value" => "Finance"}
 ].each do |param|
   Configuration.find_or_create_by_config_key_and_config_value(param)
 end
@@ -56,7 +56,7 @@ if User.first( :conditions=>{:admin=>true}).blank?
   employee_grade = EmployeeGrade.find_or_create_by_name(:name => 'System Admin',:priority => 0 ,:status => true,:max_hours_day=>nil,:max_hours_week=>nil)
 
   employee = Employee.find_or_create_by_employee_number(:employee_number => 'admin',:joining_date => Date.today,:first_name => 'Admin',:last_name => 'User',
-    :employee_department_id => employee_department.id,:employee_grade_id => employee_grade.id,:employee_position_id => employee_position.id,:employee_category_id => employee_category.id,:status => true,:nationality_id =>'76', :date_of_birth => Date.today-365, :email => 'noreply@fedena.com')
+                                                        :employee_department_id => employee_department.id,:employee_grade_id => employee_grade.id,:employee_position_id => employee_position.id,:employee_category_id => employee_category.id,:status => true,:nationality_id =>'76', :date_of_birth => Date.today-365, :email => 'noreply@fedena.com')
 
   employee.user.update_attributes(:admin=>true,:employee=>false)
 
@@ -86,15 +86,15 @@ end
 =end
 
 [
-  {"settings_key" => "ApplicationEnabled"                 ,"is_enabled" => false },
-  {"settings_key" => "ParentSmsEnabled"                   ,"is_enabled" => false},
-  {"settings_key" => "EmployeeSmsEnabled"                 ,"is_enabled" => false},
-  {"settings_key" => "StudentSmsEnabled"                  ,"is_enabled" => false},
-  {"settings_key" => "ResultPublishEnabled"               ,"is_enabled" => false},
-  {"settings_key" => "StudentAdmissionEnabled"            ,"is_enabled" => false},
-  {"settings_key" => "ExamScheduleResultEnabled"          ,"is_enabled" => false},
-  {"settings_key" => "AttendanceEnabled"                  ,"is_enabled" => false},
-  {"settings_key" => "NewsEventsEnabled"                  ,"is_enabled" => false}
+    {"settings_key" => "ApplicationEnabled"                 ,"is_enabled" => false },
+    {"settings_key" => "ParentSmsEnabled"                   ,"is_enabled" => false},
+    {"settings_key" => "EmployeeSmsEnabled"                 ,"is_enabled" => false},
+    {"settings_key" => "StudentSmsEnabled"                  ,"is_enabled" => false},
+    {"settings_key" => "ResultPublishEnabled"               ,"is_enabled" => false},
+    {"settings_key" => "StudentAdmissionEnabled"            ,"is_enabled" => false},
+    {"settings_key" => "ExamScheduleResultEnabled"          ,"is_enabled" => false},
+    {"settings_key" => "AttendanceEnabled"                  ,"is_enabled" => false},
+    {"settings_key" => "NewsEventsEnabled"                  ,"is_enabled" => false}
 
 
 ].each do |param|
